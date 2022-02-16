6f176822e26228468c2aec9a83b902f962191c80
Doomer77 <newzibkoff1984@gmail.com>
2022-02-16 18:51:17 +0300

    addeded preloader

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,24 +1,43 @@
-import React, {useEffect} from 'react';
-import './main.less'
-import {useDispatch, useSelector} from "react-redux";
-import {getRepos} from "../actions/repos";
-import Repo from "./repo/Repo";
+import React, { useEffect, useState } from 'react'
+import './Main.less'
+import { useDispatch, useSelector } from 'react-redux'
+import { getRepos } from '../actions/repos'
+import Repo from './repo/Repo'
 
 const Main = () => {
   const dispatch = useDispatch()
   const repos = useSelector(state => state.repos.items)
+  const isFetching = useSelector(state => state.repos.isFetching)
+  const [searchValue, setSearchValue] = useState('')
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(getRepos())
   }, [])
 
+  const searchHandler = () => {
+    dispatch(getRepos(searchValue))
+  }
+
   return (
     <div>
-      {repos.map(repo =>
-        <Repo repo={repo}/>
+      <div className="search">
+        <input
+          value={searchValue}
+          onChange={e => setSearchValue(e.target.value)}
+          type="text"
+          className="search-input"
+        />
+        <button onClick={() => searchHandler()} className="search-btn">
+          Search
+        </button>
+      </div>
+      {isFetching === false ? (
+        repos.map(repo => <Repo repo={repo} />)
+      ) : (
+        <div className="fetching"></div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Main;
+export default Main
